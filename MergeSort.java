261d5ce002d0e8dbbe909b14597bae29666a7425
Sunilsindur <105812476+Sunilsindur@users.noreply.github.com>
2023-04-19 08:36:50 +0530

    Update MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -52,6 +52,59 @@ public class MergeSort {
         }
         return mix;
     }
+    
+    
+    // Merge sortInplace
+    
+     static void mergeSortInPlace(int[] arr,int s,int e) {
+        if ((e-s) == 1) {
+             return;
+        }
+
+        int mid = s + (e-s) / 2;
+
+      mergeSortInPlace(arr, s, mid);
+      mergeSortInPlace(arr, mid, e);
+
+         merge(arr,s,mid,e);
+    }
+
+     static void merge(int[] arr,int s,int mid,int e) {
+        int[] mix = new int[e-s];
+
+        int i = s;
+        int j = mid;
+        int k = 0;
+
+        while (i < mid && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        while (i < mid) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+         for (int l = 0; l < mix.length; l++) {
+             arr[s+l] = mix[l];
+         }
+    }
+
 
 }
 
